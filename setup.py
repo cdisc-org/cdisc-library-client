10e7629cea2fa17662a35d163d8ba52574f43855
Nicholas Haydel <nhaydel@cdisc.org>
2022-01-27 15:21:23 -0600

    update setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
-    packages=setuptools.find_packages(include=["client, client.*"]),
+    packages=setuptools.find_packages(include=["cdisc_library_client, cdisc_library_client.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
